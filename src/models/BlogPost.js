5fb126dee56909928b8d7f0a9c5b0ba309434288
Edson Pereira <ed.1206@gmail.com>
2022-12-01 19:37:23 -0300

    feat: :sparkles: Criação do modelo `BlogPost`

---
+++ src/models/BlogPost.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: { type: DataTypes.INTEGER, primaryKey: true },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: DataTypes.INTEGER,
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+  },
+  {
+    tableName: 'blog_posts',
+    timestamps: false,
+    underscored: true,
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User,
+      { foreignKey: 'user_id', as: 'user' });
+  };
+
+  return BlogPost;
+}
--- src/models/User.js
+++ src/models/User.js
@@ -11,5 +11,10 @@ module.exports = (sequelize, DataTypes) => {
     underscored: true,
   });
 
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost,
+      { foreignKey: 'user_id', as: 'blog_posts'});
+  }
+
   return User;
 }
